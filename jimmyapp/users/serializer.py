39de7052decd649423b4163ef8b304f48c3529ca
stopplay <Jimmy@arbid.com.br>
2022-02-01 01:07:10 +0000

    new

--- jimmyapp/users/serializer.py
+++ jimmyapp/users/serializer.py
@@ -6,17 +6,17 @@ from users.models import Address, Establishment, User
 class UserSerielizer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['user_id', 'name','email', 'address']
+        fields = ['id', 'name','email', 'address']
 
 
 class EstablishmentSerielizer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Establishment
-        fields = ['establishment_id', 'name','email', 'address']
+        fields = ['id', 'name','email', 'address']
 
 
 
 class AddressSerielizer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Address
-        fields = ['address_id', 'address_line_1', 'address_line_2', 'town', 'city', 'post_code']
+        fields = ['id', 'address_line_1', 'address_line_2', 'town', 'city', 'post_code']
